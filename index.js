679855bdac884de96dd3eea3b11a33f11b7bc0a6
Jalal Azimi <nazari.samaneh@gmail.com>
2016-10-02 23:01:31 +0330

    update project

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-'use strict'
+var express = require('express');
+var app = express();
 
-let numberArray = [2, 34, 451, 2, 234, 5, 68, 81, 0];
-
-function sort(array) {
-	return array.sort();
-}
-
-function duplicateArray(array) {
-	return numberArray.map(val => {
-		return val * 2;
-	});
-}
-
-function checkNumber(array) {
-	return array.map(val => {
-		return !(val % 2) ? 0 : 1;
-	})
-}
-
-numberArray.map(function(val, index) {
-console.log(val,index);
-})
-
-//console.log(duplicateArray(numberArray));
-console.log(checkNumber(numberArray));
+app.get('/',function (req, res){
+    res.send('Hello World')
+});
+app.listen(3000);
