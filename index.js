a5b1616e854f9078f939a747b86d438756c7946c
sama <sama@Jalals-MacBook-Pro.local>
2016-10-03 23:24:39 +0330

    learn express routes

--- index.js
+++ index.js
@@ -4,4 +4,19 @@ var app = express();
 app.get('/',function (req, res){
     res.send('Hello World')
 });
-app.listen(3000);
+
+app.post('/',function(req, res){
+    res.send('Got a post request');
+});
+app.put('/user',function (req,res) {
+    res.send('Got a put request at /user');
+});
+app.delete('/user',function(req,res){
+    res.send('Got a delete request at /user');
+});
+app.use(function(req, res, next){
+    res.status(404).send('error')
+});
+app.listen(3000, function(){
+    console.log('Start')
+});
